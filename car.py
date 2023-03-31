62dfa2c35409d8f5035c6a5b4a9b84e7b4d736a2
Zeshan <zeshan-haider@hotmail.co.uk>
2023-04-01 01:34:16 +0500

    refactored the specific engine classes

--- car.py
+++ car.py
@@ -2,10 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class Car(ABC):
-    def __init__(self, last_service_date, engine, battery):
-        self.last_service_date = last_service_date
+    def __init__(self, engine, battery):
         self.engine = engine
         self.battery = battery
+        
 
     @abstractmethod
     def needs_service(self):
