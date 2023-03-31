edf93a12e689e2e92d2b2dec5911ebc3d01ad303
Zeshan <zeshan-haider@hotmail.co.uk>
2023-04-01 01:13:51 +0500

    updated car.py to include engine and battery atr's

--- car.py
+++ car.py
@@ -2,8 +2,10 @@ from abc import ABC, abstractmethod
 
 
 class Car(ABC):
-    def __init__(self, last_service_date):
+    def __init__(self, last_service_date, engine, battery):
         self.last_service_date = last_service_date
+        self.engine = engine
+        self.battery = battery
 
     @abstractmethod
     def needs_service(self):
