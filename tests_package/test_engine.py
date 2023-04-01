6ec5e789a9a77970d5fea4c7f4fd13bb48c51b2a
Zeshan <zeshan-haider@hotmail.co.uk>
2023-04-02 03:38:39 +0500

    Capulet engine unit test designed and implemented, working

--- tests_package/test_engine.py
+++ tests_package/test_engine.py
@@ -13,38 +13,38 @@ class CapuletEngineTesting(unittest.TestCase):
         last_service_mileage = 0
 
         engine = CapuletEngine(current_mileage, last_service_mileage)
-        self.assertFalse(CapuletEngine.needs_service())
+        self.assertFalse(engine.needs_service())
 
     def test_capuletEngine_noService_2(self): # testing mileage values different
         current_mileage = 11
         last_service_mileage = 10
 
         engine = CapuletEngine(current_mileage, last_service_mileage)
-        self.assertFalse(CapuletEngine.needs_service())
+        self.assertFalse(engine.needs_service())
 
     def test_capuletEngine_noService_3(self): # testing mileage values on threshold
         current_mileage = 51235
         last_service_mileage = 21235
 
         engine = CapuletEngine(current_mileage, last_service_mileage)
-        self.assertFalse(CapuletEngine.needs_service())
+        self.assertFalse(engine.needs_service())
 
     """
     Testing CapuletEngine.needs_service() to return True
     """
-    def test_capuletEngine_noService_2(self): # testing mileage values over threshold
+    def test_capuletEngine_service_1(self): # testing mileage values over threshold
         current_mileage = 112546
         last_service_mileage = 31920
 
         engine = CapuletEngine(current_mileage, last_service_mileage)
-        self.assertTrue(CapuletEngine.needs_service())
+        self.assertTrue(engine.needs_service())
 
-    def test_capuletEngine_noService_3(self): # testing mileage values on threshold
-        current_mileage = 102392
+    def test_capuletEngine_service_2(self): # testing mileage values on threshold
+        current_mileage = 112392
         last_service_mileage = 82391
 
         engine = CapuletEngine(current_mileage, last_service_mileage)
-        self.assertTrue(CapuletEngine.needs_service())   
+        self.assertTrue(engine.needs_service())   
 
 if __name__ == '__main__':
     unittest.main()
