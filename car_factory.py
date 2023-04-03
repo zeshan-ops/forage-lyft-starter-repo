efedca2d4969fee7d3b612e5130f96ff3f2e2e6c
Zeshan <zeshan-haider@hotmail.co.uk>
2023-04-03 21:59:16 +0500

    carfactory update commited, accidentally missed in previous commit

--- car_factory.py
+++ car_factory.py
@@ -6,21 +6,21 @@ from battery.spindler_battery import SpindlerBattery
 from battery.nubbin_battery import NubbinBattery
 
 class CarFactory(Car):
-    def create_calliope(last_service_date, current_mileage, last_service_mileage):
+    def create_calliope(current_date,last_service_date, current_mileage, last_service_mileage):
         return Car(CapuletEngine(current_mileage, last_service_mileage),
-            SpindlerBattery(last_service_date))
+            SpindlerBattery(last_service_date, current_date))
     
-    def create_glissade(last_service_date, current_mileage, last_service_mileage):
+    def create_glissade(current_date,last_service_date, current_mileage, last_service_mileage):
         return Car(WilloughbyEngine(current_mileage,last_service_mileage),
-            SpindlerBattery(last_service_date))
+            SpindlerBattery(last_service_date,current_date))
     
     def create_palindrome(current_date, last_service_date, warning_light_is_on):
-        return Car(SternmanEngine(warning_light_is_on), SpindlerBattery(last_service_date))
+        return Car(SternmanEngine(warning_light_is_on), SpindlerBattery(last_service_date,current_date))
     
     def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
         return Car(WilloughbyEngine(current_mileage,last_service_mileage),
-            NubbinBattery(last_service_date))
+            NubbinBattery(last_service_date, current_date))
     
     def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
         return Car(CapuletEngine(current_mileage,last_service_mileage),
-            NubbinBattery(last_service_date))
+            NubbinBattery(last_service_date, current_date))
